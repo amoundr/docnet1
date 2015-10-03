670ee2bcd328ccde0cd68b823913b598448a59e1
unknown <am.nador@gmail.com>
2015-10-03 02:57:51 +0200

    add mongo to production file

--- config/env/production.js
+++ config/env/production.js
@@ -8,10 +8,10 @@ module.exports = {
   },
   port: process.env.PORT || 8443,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri:  'ds051943.mongolab.com:51943/amoundrtest' ,    //process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
     options: {
-      user: '',
-      pass: ''
+      user: 'amoundr',
+      pass: 'Aa.123456'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
